bc5dc61311fd087e0d90dbe678eb518e63cc05c8
Artem Ivanchenko <imcmib@gmail.com>
2025-01-02 12:13:03 -0600

    Initial commit

---
+++ app/src/main/java/com/aivanchenko/fetch/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.aivanchenko.fetch.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.aivanchenko.fetch.app.ui.theme.FetchRewardsCodingExerciseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FetchRewardsCodingExerciseTheme {
+
+            }
+        }
+    }
+}
